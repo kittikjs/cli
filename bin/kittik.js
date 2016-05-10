a3ce80f173f342a9ecae441ab8fe3673309f7537
Eugene Obrezkov <ghaiklor@gmail.com>
2016-05-10 14:20:14 +0300

    chore(package): Remove version information from CLI

--- bin/kittik.js
+++ bin/kittik.js
@@ -2,10 +2,8 @@
 "use strict";
 
 const commander = require('commander');
-const pkg = require('../package.json');
 
 commander
-  .version(pkg.version)
   .command('start [file]', 'Start a presentation')
   .command('create [file]', 'Create a new presentation')
   .parse(process.argv);
